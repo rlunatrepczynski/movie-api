f92ca0a36a7b1155d065dcf1e9532c370db43854
crene <crenee72@hotmail.com>
2023-12-14 22:47:37 -0700

    Update index.js
    Updated index.js for exercise 2.4

--- index.js
+++ index.js
@@ -1,2 +1,71 @@
-const express = require('express');
+const express = require('express'),
+    fs = require('fs'),
+    morgan = require('morgan'),
+    path = require('path');
+
 const app = express();
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' });
+
+let topTenMovies = [
+    {
+        title: 'Happy Gilmore',
+        diretor: 'Dennis Dugan'
+    },
+    {
+        title: 'Mean Girls',
+        director: 'Mark Waters'
+    },
+    {
+        title: 'Twilight',
+        director: 'Catherine Hardwicke'
+    },
+    {
+        title: 'Jurassic Park',
+        director: 'Steven Spielberg'
+    },
+    {
+        title: 'Gladiator',
+        director: 'Ridley Scott'
+    },
+    {
+        title: 'Alien',
+        director: 'Ridley Scott'
+    },
+    {
+        title: 'The Departed',
+        director: 'Martin Scorsese'
+    },
+    {
+        title: 'Harry Potter and the Sorcer\'s Stone',
+        director: 'Chris Columbus'
+    },
+    {
+        title: 'Spirited Away',
+        director: 'Hayao Miyazaki'
+    },
+    {
+        title: 'Everything Everywhere All at Once',
+        director: 'Daniel Kwan, Daniel Scheinert'
+    }
+]
+
+app.use(morgan('combined', { stream: accessLogStream }));
+app.use(express.static('public'));
+
+app.get('/movies', (req, res) => {
+    res.json(topTenMovies);
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Movie Flix')
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something Broke!');
+});
+
+app.listen(8080, () => {
+    console.log('The movie app has loaded and is listening on port 8080');
+});
