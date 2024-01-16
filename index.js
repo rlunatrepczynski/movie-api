5dcb3d1511e0ddad0c8df2323ada6d8b32a77924
crene <crenee72@hotmail.com>
2024-01-15 22:08:47 -0700

    Update index.js
    Updated mongoose.connect to connection_uri for Atlas.

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ const cors = require('cors');
 const { check, validationResult } = require('express-validator');
 
 
-mongoose.connect("mongodb://localhost:27017/cfDB", {
+/* mongoose.connect("mongodb://localhost:27017/cfDB", {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+    useUnifiedTopology: true
+}); */
+
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let allowedOrigins = ['http//localhost:8080', 'http://testsite.com'];
 app.use(cors({
